e10248935cd12494f6a6dbf06a3795a0a9506472
Mariel Adrianna de la Garza <mariel.code@gmail.com>
2019-01-14 01:30:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word_forwards = word 
+  end 
+  
+  def match(array)
+    array.select do |word|
+      word.split("").sort == @word_forwards.split("").sort 
+    end 
+  end 
+  
+end 
